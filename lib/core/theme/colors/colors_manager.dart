0c0a14aea80a4b2c53e88ff5c8b591bd1c49044d
Bastawy <mohamed.bastawiie@gmail.com>
2026-08-03 17:32:04 +0300

    Add colors and typography

---
+++ lib/core/theme/colors/colors_manager.dart
@@ -0,0 +1,12 @@
+import 'dart:ui';
+
+class ColorsManager {
+  static const Color amber = Color(0xFFF0A202);
+  static const Color teal = Color(0xFF15181A);
+  static const Color darkGrey = Color(0xFF15181A);
+  static const Color lightGrey = Color(0xFF1B1F22);
+  static const Color sage = Color(0xFF7FA66B);
+  static const Color red = Color(0xFFD9614F);
+  static const Color white = Color(0xFFF2EFE9);
+  static const Color whiteGrey = Color(0xFF8B959B);
+}
